9a6889070348d8f2a27b183c105198113f88c4b1
christophe4691 <doignon.christophe@orange.fr>
2018-05-23 16:54:33 +0200

    Ajout de code dans index, script, style et nouveau fichier json

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+//RECUPERER LA CARTE DE CAHORS
+
+var map = L.map('mapid', { zoomControl: false }).setView([44.4475229, 1.441989], 13);
+
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
 
+//ON PARSE LE FICHIER JSON EN JAVA SCRIPT
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+$.getJSON('places.json', function(cats) {    
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+	for (var i = 0; i < cats.length; i++)
+	{
+		console.log(cats[i].name);
+
+
+		var children = cats[i].children;
+
+		for (var j = 0; j < children.length; j++) 
+		{
+
+
+			var places = children[j].places;
+
+			for( var k = 0; k < places.length; k++)
+			{
+				// console.log(places[k].lat);
+				// console.log(places[k].lon);
+
+				L.marker([places[k].lat, places[k].lon])
+				.bindPopup('<h3>' + places[k].name + '</h3> <p>' + places[k].description + '</p>')
+				.addTo(map);
+			}
+
+		var menu = children[j];
+		document.getElementByTagName('sidebar-brand').innerHTML = menu;	
+		}		
+	}
+
+});
+
+
+
+
+
+
+//SCRIPT POUR LE MENU TOGGLE
+
+$("#menu-toggle").click(function(e)
+{
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+});
+
+
+//FERMETURE DU MENU TOGGLE
+
+$('#sidebar-wrapper a').on("click", function(){
+	$('#sidebar-wrapper').collapse('hide');
+});
